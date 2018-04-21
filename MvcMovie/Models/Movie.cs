8a1e1e7b5910eb41976e4b97e5d06357b432cec6
Hiroaki Tomiyoshi <htomiyosi@icloud.com>
2018-04-21 23:22:49 +0900

    https://docs.microsoft.com/ja-jp/aspnet/core/tutorials/first-mvc-app-mac/validation 2
    1行に複数の属性を組み合わせる

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -9,22 +9,16 @@ namespace MvcMovie.Models
         [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        [Range(1, 100)]
-        [DataType(DataType.Currency)]
-        public decimal Price { get; set; }
-
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Required]
-        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
+        [Range(1, 100), DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), StringLength(5)]
         public string Rating { get; set; }
     }
 }
